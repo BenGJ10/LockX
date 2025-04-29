783f9a3d72f4f3d8b18bc20584451491d0a2b171
Pooja-9703 <143784271+Pooja-9703@users.noreply.github.com>
2025-04-29 22:42:06 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-# main.py
-# Entry point for the Process Monitor application
 
-import tkinter as tk
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from gui.interface import ProcessMonitorApp
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = ProcessMonitorApp(root)
-    root.mainloop()
